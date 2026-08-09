b0bdaa23360977ee12d8dc9724ded202aeb8eac8
HeorhiiOzz <bozhnyukg@gmail.com>
2026-08-09 13:43:15 +0300

    Add dynamic SEO sitemap

---
+++ api/sitemap.js
@@ -0,0 +1,18 @@
+const {readPublicCars}=require('../lib/publicCatalog');
+const xml=s=>String(s??'').replace(/[<>&'\"]/g,m=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','\"':'&quot;'}[m]));
+module.exports=async(req,res)=>{
+  try{
+    if(req.method!=='GET') return res.status(405).end('Method not allowed');
+    const cars=await readPublicCars();
+    const base='https://solodealer.store';
+    const fixed=['/','/avto-pid-vyplatu','/avto-v-kredyt','/anketa'];
+    const urls=[...fixed,...cars.map(c=>`/auto/${encodeURIComponent(c.id)}`)];
+    const body=`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.map(u=>`  <url><loc>${xml(base+u)}</loc></url>`).join('\n')}\n</urlset>`;
+    res.setHeader('Content-Type','application/xml; charset=utf-8');
+    res.setHeader('Cache-Control','public, max-age=0, s-maxage=3600');
+    return res.status(200).send(body);
+  }catch(e){
+    console.error('sitemap',e);
+    return res.status(500).send('Sitemap error');
+  }
+};
